9a3435517f7089c8b5e38434b7bcfb993cd4b553
Gekif <dzulfikar.maulana@gmail.com>
2020-03-29 17:55:22 +0700

    2. Basic learning: 3. Controllers in CodeIgniter

---
+++ application/controllers/Blog.php
@@ -0,0 +1,15 @@
+<?php
+
+class Blog extends CI_Controller
+{
+    public function index()
+    {
+        echo 'This is index function of blog controller';
+    }
+
+
+    public function add()
+    {
+        echo "Add function of blog controller";
+    }
+}
